bbe83d08d0a94f2498cefbda8646752f66bd88a1
Patrick Flügge <Patrick.fluegge@mailbox.org>
2019-02-28 14:15:05 +0100

    Text Areas & Selects + Config #2

--- src/views/form-inputs/value.blade.php
+++ src/views/form-inputs/value.blade.php
@@ -1,4 +1,8 @@
 @include('partial-elements::base.input',[
     'name' => 'value',
-    'type' => 'password'
+    'type' => 'number',
+    'pattern' => '[0-9]+([\.,][0-9]+)?"',
+    'step' => '0.01',
+    'min' => '0.01',
+    'max' => '20000000',
 ])
