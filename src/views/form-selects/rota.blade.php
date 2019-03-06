9627c4d7174c5d615b07284fb8504fb525265ff9
Patrick Flügge <Patrick.fluegge@mailbox.org>
2019-03-06 14:31:56 +0100

    Bugfix

--- src/views/form-selects/rota.blade.php
+++ src/views/form-selects/rota.blade.php
@@ -4,8 +4,8 @@
         ['value' => 1, 'description' => 'Monatlich'],
         ['value' => 3, 'description' => 'Vierteljährlich'],
         ['value' => 6, 'description' => 'Halbjährlich'],
-        ['value' => 12, 'description' => 'Jährlich']
-        ['value' => 24, 'description' => 'Zweijährlich']
-        ['value' => 36, 'description' => 'Dreijährlich']
+        ['value' => 12, 'description' => 'Jährlich'],
+        ['value' => 24, 'description' => 'Zweijährlich'],
+        ['value' => 36, 'description' => 'Dreijährlich'],
     ]
 ])
